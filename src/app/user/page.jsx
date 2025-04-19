073d80b4faddfaf5d17fba10e1135f71f8282308
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-04-19 11:14:42 +0530

    the global css for the full screen

---
+++ src/app/user/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='text-red-500 h-full flex justify-center items-center pt-8 border bg-white'>Ikhlas is a user page</div>
+  )
+}
+
+export default page
